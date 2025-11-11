cc7c1d1e284ef5186fa46dbec1035d88e231e789
Rob <148229632+itsRobix@users.noreply.github.com>
2025-11-11 12:02:29 +0200

    fixed min value

--- C27/C27.cpp
+++ C27/C27.cpp
@@ -14,8 +14,8 @@ int maxv(int a[], int n){                       //max vērtības atrašana
     int *results = new int[n*2]; //šeit tiks saglabāti rezultāti
 
     for(int i = 0; i<n; i++){  //tiek uztaisīts lists ar visām vērtībām no 1 līdz n*2
-        results[i] = a[i+1] + a[n*2-i];
-        cout << "results[" << i << "] = " << a[i+1] << " + " << a[n*2-i] << " = " << results[i] << endl; //Paziņo programmas darbības un to rezultātus
+        results[i] = a[i] + a[n*2-i-1];
+        cout << "results[" << i << "] = " << a[i] << " + " << a[n*2-i-1] << " = " << results[i] << endl; //Paziņo programmas darbības un to rezultātus
     }
 
     maxVal = results[0];
@@ -24,6 +24,7 @@ int maxv(int a[], int n){                       //max vērtības atrašana
     }
 
     cout << " Max value = " << maxVal << endl;   //rezultāti
+    delete [] results;
     return(maxVal);
 }
 
@@ -33,16 +34,19 @@ int minv(int a[], int n){                     //mazākās vērtības atrašana
     int *results = new int[n*2];
 
     for(int i = 0; i<n; i++){  //tiek uztaisīts lists ar visām vērtībām no 1 līdz n*2
-        results[i] = a[i+1] * a[n+i+1];
-        cout << "results[" << i << "] = " << a[i+1] << " * " << a[n+i+1] << " = " << results[i] << endl; //Paziņo programmas darbības un to rezultātus
+        results[i] = a[i] * a[n+i];
+        cout << "results[" << i << "] = " << a[i] << " * " << a[n+i] << " = " << results[i] << endl; //Paziņo programmas darbības un to rezultātus
     }
 
     minVal = results[0];
     for(int i = 1; i<n; i++){
-        if(results[i] > results[i-1]) minVal = results[i];      //nosaka mazāko rezultātu
+        if(results[i] < minVal){
+            minVal = results[i];      //nosaka mazāko rezultātu
+        } 
     }
 
     cout << " Min value = " << minVal << endl; //rezultāti
+    delete [] results;
     return(minVal);
 }
 
@@ -53,20 +57,21 @@ int main(){
         int usrNumInList;
         cout << "Ievadiet n vertibu: ";
         cin >> usrValue; //pieņemsim, ka ievada 3
-        int n2 = usrValue*2; //n = 6
+        int n2 = usrValue*2-1; //n = 6
         int *a = new int[n2]; //dinamiskais masīvs ar 2n vietām
+        a[0] = 0; //Pirmā [0] pozīcija listā tiek iestatīta kā 0, lai programmu varētu atkārtot bez kļūdām
 
         int atkartot = 0; //mainīgais programmas atkārtošanai
 
-        for(int i = 1; i<=n2; i++){  //tiek uztaisīts lists ar visām vērtībām no 1 līdz n*2
-            cout << "Ievadiet " << i << ". skaitli: ";
+        for(int i = 0; i<=n2; i++){  //tiek uztaisīts lists ar visām vērtībām no 1 līdz n*2
+            cout << "Ievadiet " << i+1 << ". skaitli: ";
             cin >> usrNumInList;
             a[i] = usrNumInList;
         //    cout << a[i] << endl; //-----------------testing only
         }
 
         cout << "Jusu skaitlu virkne: ";
-        for(int i = 1; i<=n2; i++){
+        for(int i = 0; i<=n2; i++){
             cout << a[i] << ", ";
         }
         cout << endl;
